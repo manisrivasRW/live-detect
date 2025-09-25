7bf030045874eb5266f0a2dea73fd77d97bd734c
Ratish-RW <ratish.m@randomwalk.ai>
2025-09-25 17:42:37 +0530

    fb

--- frontend/.next/server/app/api/video_feed/[stream_id]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/video_feed/[stream_id]/route.js")
-R.c("server/chunks/node_modules_next_dist_4cde1a6a._.js")
-R.c("server/chunks/[root-of-the-server]__4cd72954._.js")
-R.m("[project]/.next-internal/server/app/api/video_feed/[stream_id]/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/video_feed/[stream_id]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/video_feed/[stream_id]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
